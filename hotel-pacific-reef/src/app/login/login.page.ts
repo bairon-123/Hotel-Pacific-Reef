8a36568956f29ad87a7908337677c4ad0c64a64d
solrack1414 <carl.varela@duocuc.cl>
2025-09-29 02:34:19 -0300

    se borran 2 import que no se usan

--- hotel-pacific-reef/src/app/login/login.page.ts
+++ hotel-pacific-reef/src/app/login/login.page.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ToastController, NavController, IonInput } from '@ionic/angular';
 import { AuthDbService } from '../services/auth-db.service';
-import { TranslationService } from '../services/translation.service';
-import { TranslatePipe } from '../pipes/translate.pipe';
+
 
 @Component({
   selector: 'app-login',
